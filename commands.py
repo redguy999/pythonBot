6c5e66f65e6a750a8b086b1d3c4964397af28358
redguy999 <Bigredguy05@hotmail.com>
2022-05-16 15:07:07 -0400

    adds search command (not done)

--- commands.py
+++ commands.py
@@ -3,3 +3,27 @@
 async def highFive(ctx,name):
   await ctx.send(f'You give {name} a high five!')
 
+def ManURL(rl):
+    url = rl
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+def getLinked():
+    tags = soup.find_all('a')
+    linksFound = []
+    for x in tags:
+      linksFound.append(x['href'])
+    return linksFound
+
+@bot.command()
+async def search(ctx,term,entry):
+  try:
+    entry=int(entry)
+  except:
+    entry=0
+  #id=rcnt
+  #search "term" on google, and return the url of the result matching the entry number.
+  url=f"https://www.google.com/search?q={term}"
+  global soup
+  soup = ManURL(url)
+  giveBack=getLinked()[entry]
+  await ctx.send(giveBack)
