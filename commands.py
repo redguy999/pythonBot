6ba88d06b2533719dbb37ddd5128287de699d531
redguy999 <Bigredguy05@hotmail.com>
2022-05-26 14:31:18 -0400

    makes search work.

--- commands.py
+++ commands.py
@@ -17,23 +17,30 @@ def getLinked(parent):
     return linksFound
 
 @bot.command()
-async def search(ctx,term,entry):
+async def search(ctx,entry,*term):
+  searchTerm =""
+  for word in term:
+    searchTerm+=word
   try:
     entry=int(entry)
   except:
     entry=0
   #id=rcnt
   #search "term" on google, and return the url of the result matching the entry number.
-  url=f"https://www.google.com/search?q={term}"
+  url=f"https://www.google.com/search?q={searchTerm}"
   global soup
   soup = ManURL(url)
   #insert getting just whats suppose to be the results here, if you can get that working.
   links=getLinked(soup)
+  print(links)
   for link in links.copy():
     if re.findall("google",link) or not re.findall("http.*:",link):
       links.remove(link)
   for num in range(0,len(links)):
-    links[num]=re.findall("/url?q=(.+)&sa=U&ved=",links[num])[0]
+    try:
+      links[num]=re.findall("(.+)&sa=U&ved=",links[num])[0]
+    except:
+      print(f"re.findall failed to work on: {links[num]}")
   await ctx.send(links[entry])
 
 @bot.command()
